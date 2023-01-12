4fedddb6cd20f1535126801d217c38393a06e017
Abid Usmani <abidusmani343@gmail.com>
2023-01-12 22:22:37 +0530

    added multiply.java

---
+++ multiply.java
@@ -0,0 +1,7 @@
+piblic class Multiply{
+    public  static void main(String [] args){
+        int a=5;
+        int b=3;
+        System.out.println("difference is followed by you"+(a*b));
+    }
+}
