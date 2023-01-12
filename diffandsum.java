3b65fa3b4a0340146f9995a33a7f1f44a208bc03
Abid Usmani <abidusmani343@gmail.com>
2023-01-12 19:25:02 +0530

    diffsum.java commit

---
+++ diffandsum.java
@@ -0,0 +1,7 @@
+piblic class DiffSum{
+    public  static void main(String [] args){
+        int a=5;
+        int b=3;
+        System.out.println("difference is followed by you"+(a-b)+(a+b));
+    }
+}
